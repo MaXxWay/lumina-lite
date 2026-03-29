fb5b0e16f6c5c9e8fb40ca2d501694e6d664b9b2
MaXxWay <maxxway.yt@gmail.com>
2026-03-29 22:58:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://ofxvazqurjgnxxuozjlr.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHZhenF1cmpnbnh4dW96amxyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NDYxODg4MSwiZXhwIjoyMDkwMTk0ODgxfQ.geqs7rK3DBQHLmKmEOjOASDQttIEHdgFt1yFjclYCBo';
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 let currentUser = null;
