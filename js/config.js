f134e54115040e115f974381c5115c33c07b22ba
MaXxWay <maxxway.yt@gmail.com>
2026-03-29 20:30:48 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 export const SUPABASE_URL = 'https://ofxvazqurjgnxxuozjlr.supabase.co';
 export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHZhenF1cmpnbnh4dW96amxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTg4ODEsImV4cCI6MjA5MDE5NDg4MX0.Zf2pwQNmxe9wBt7tlZed-ntnLPzm7JGOuqkLuBkv0GE';
 
+// Только здесь создаем клиент!
 export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const BOT_USER_ID = '00000000-0000-0000-0000-000000000000';
@@ -8,7 +9,6 @@ export const BOT_PROFILE = {
     id: BOT_USER_ID,
     username: 'lumina_bot',
     full_name: 'Lumina Bot',
-    bio: 'Официальный бот мессенджера Lumina Lite',
     is_bot: true
 };
 
