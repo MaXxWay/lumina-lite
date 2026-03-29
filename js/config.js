e623767bcbfb5fe79383e941f7d76b360e1a92a6
MaXxWay <maxxway.yt@gmail.com>
2026-03-29 22:22:02 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,6 @@
+// config.js
 export const SUPABASE_URL = 'https://ofxvazqurjgnxxuozjlr.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHZhenF1cmpnbnh4dW96amxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTg4ODEsImV4cCI6MjA5MDE5NDg4MX0.Zf2pwQNmxe9wBt7tlZed-ntnLPzm7JGOuqkLuBkv0GE';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meXZhenF1cmpnbnh4dW96amxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTg4ODEsImV4cCI6MjA5MDE5NDg4MX0.Zf2pwQNmxe9wBt7tlZed-ntnLPzm7JGOuqkLuBkv0GE';
 
 export const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
@@ -12,4 +13,5 @@ export const BOT_PROFILE = {
     is_bot: true
 };
 
+// Функция для генерации email из username
 export const getEmail = (u) => `${u.toLowerCase().trim().replace(/^@/, '')}@lumina.local`;
