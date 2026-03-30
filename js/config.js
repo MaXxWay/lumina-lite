27c3ca2a407c027d805116bcccd9b696faaca70f
MaXxWay <maxxway.yt@gmail.com>
2026-03-31 00:24:32 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,11 +2,9 @@
 const SUPABASE_URL = 'https://ofxvazqurjgnxxuozjlr.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meHZhenF1cmpnbnh4dW96amxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTg4ODEsImV4cCI6MjA5MDE5NDg4MX0.Zf2pwQNmxe9wBt7tlZed-ntnLPzm7JGOuqkLuBkv0GE';
 
-// ID специальных чатов
 const BOT_USER_ID = '00000000-0000-0000-0000-000000000000';
 const SAVED_CHAT_ID = 'ffffffff-ffff-ffff-ffff-ffffffffffff';
 
-// Профили специальных пользователей
 const BOT_PROFILE = {
     id: BOT_USER_ID,
     username: 'lumina_bot',
